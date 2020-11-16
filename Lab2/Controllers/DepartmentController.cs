4d49977080e890d5d6f061f71c8a872b5875072f
Amr Fasseeh <amr_m_fasseeh@hotmail.com>
2020-11-16 18:44:46 +0200

    Lab2

---
+++ Lab2/Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab2.Models;
+
+namespace Lab2.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        static List<Department> departments = new List<Department>()
+        {
+            new Department() {Dept_id = 1, Dept_name = "IT"},
+            new Department() {Dept_id = 2, Dept_name = "Sales"},
+            new Department() {Dept_id = 3, Dept_name = "Marketing"},
+            new Department() {Dept_id = 4, Dept_name = "Production"}
+        };
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View(departments);
+        }
+
+        public ActionResult CreateView()
+        {
+            return View();
+        }
+
+        public ActionResult Create(Department dept)
+        {
+            departments.Add(dept);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditView(int id)
+        {
+            Department dept = departments.FirstOrDefault(d => d.Dept_id == id);
+            return View(dept);
+        }
+
+        public ActionResult Edit(Department dept, int id)
+        {
+            Department Edited_dept = departments.FirstOrDefault(d => d.Dept_id == id);
+            Edited_dept.Dept_name = dept.Dept_name;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Department Deleted_dept = departments.FirstOrDefault(d => d.Dept_id == id);
+            departments.Remove(Deleted_dept);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lab2/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab2.Models;
+
+namespace Lab2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        static List<Student> students = new List<Student>()
+        {
+            new Student() {id = 1, name = "Amr", age = 25},
+            new Student() {id = 2, name = "Ahmed", age = 35},
+            new Student() {id = 3, name = "Sherif", age = 40},
+            new Student() {id = 4, name = "Mohamed", age = 20}
+        };
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View(students);
+        }
+
+        public ActionResult CreateView()
+        {
+            return View();
+        }
+
+        public ActionResult Create(Student stud)
+        {
+            students.Add(stud);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditView(int id)
+        {
+            Student stud = students.FirstOrDefault(d => d.id == id);
+            return View(stud);
+        }
+
+        public ActionResult Edit(Student stud, int id)
+        {
+            Student Edited_stud = students.FirstOrDefault(d => d.id == id);
+            Edited_stud.name = stud.name;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Student Deleted_stud = students.FirstOrDefault(d => d.id == id);
+            students.Remove(Deleted_stud);
+            return RedirectToAction("Index");
+        }
+    }
+}
